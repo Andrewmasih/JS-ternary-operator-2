7110afe253df39aed76df532f63e6193a7552515
Andrew Masih <armasih@outlook.com>
2021-05-07 16:26:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+/* since 25 is greater than 18 the first expression will run,we could of wriiten the same code for 'if' 'else' statement */
+
+let age = 25;
+
+(age > 18) ?  "You are soooo mature": "You are still a child";
+
+// The code below is the same logic but written longhand
+if (age > 30) {
+  "You are soooo mature"
+} else {
+  "You are still a child"
+}
